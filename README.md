0b3277b31f0f55b16d9f6570ca14753ed7bd34e2
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-15 09:27:27 -0700

    add prototypes to return customer name and pizza topping selections

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-Program will allow user to select multiple toppings for their Pizza
-Program will allow user to select a size for their Pizza
+Program will allow user to select multiple toppings for their Pizza DONE
+Program will allow user to select a size for their Pizza DONE
+Program will return all selected toppings to the order details
 Program will determine final cost based on their selections
 Program will return the value of the cost to the user
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,37 @@
 // Back End Logic
-function Pizza(toppings, size) {
+function Pizza(first, last, toppings, size) {
+  this.firstName = first;
+  this.lastName = last;
   this.toppings = toppings;
   this.size = size;
+  // var cost = this.determineCost(toppings, size);
+  // this.cost = cost;
+}
+//
+// Pizza.prototype.determineCost() {
+//   var cost = 10;
+// }
+
+Pizza.prototype.getCustomerName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+Pizza.prototype.getToppings = function() {
+  return this.toppings + " ";
 }
 
 // Front End Logic
 $(document).ready(function() {
-  $("#submit-order-button").submit(function() {
-    var customerName = $("#input-name").val();
-    var pizzaToppings = $("#select-toppings :selected").text();
+  $("#submit-order-button").click(function(event) {
+    event.preventDefault();
+    var customerFirstName = $("#input-first-name").val();
+    var customerLastName = $("#input-last-name").val();
+    var pizzaToppings = $("#select-toppings").val();
     var pizzaSize = $("#select-size :selected").text();
-    var newPizza = new Pizza(pizzaToppings, pizzaSize);
-    $("#customer-name").text(newTicket.movie);
-    $("#show-time").text(newTicket.mTime);
-    $("#movie-rating").text(newTicket.mRating);
-    $("#total-cost").text(newTicket.cost);
+    var newPizza = new Pizza(customerFirstName, customerLastName, pizzaToppings, pizzaSize);
+    $("#customer-name").text(newPizza.getCustomerName());
+    $("#chosen-toppings").text(newPizza.getToppings());
+    $("#chosen-size").text(newPizza.pizzaSize);
+    $("#total-cost").text(newPizza.cost);
   });
 });
