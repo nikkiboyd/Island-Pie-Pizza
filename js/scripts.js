93e480c78b3dd28ce368dd7450ba0bfee4178797
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-15 09:56:55 -0700

    add formula to increase cost withbased on topping selection

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,45 @@ function Pizza(first, last, toppings, size) {
   this.lastName = last;
   this.toppings = toppings;
   this.size = size;
-  // var cost = this.determineCost(toppings, size);
-  // this.cost = cost;
+  var cost = this.determineCost(toppings, size);
+  this.cost = cost;
 }
-//
-// Pizza.prototype.determineCost() {
-//   var cost = 10;
-// }
+
+Pizza.prototype.determineCost = function(toppings) {
+  var cost = 10;
+  if (this.toppings.length === 1) {
+    cost = cost + 2;
+  } else if (this.toppings.length === 2) {
+    cost = cost + 4;
+  } else if (this.toppings.length === 3) {
+    cost = cost + 6;
+  } else if (this.toppings.length === 4) {
+    cost = cost + 8;
+  } else if (this.toppings.length === 5) {
+    cost = cost + 10;
+  }
+  return cost;
+
+  if (this.size = "Medium") {
+    cost = cost + 2;
+  } else if (this.size = "Large") {
+    cost = cost + 4;
+  }
+  
+  return cost;
+}
 
 Pizza.prototype.getCustomerName = function() {
   return this.firstName + " " + this.lastName;
 }
 
 Pizza.prototype.getToppings = function() {
-  return this.toppings + " ";
+  var toppings = this.toppings.join(', ');
+  return toppings;
+}
+
+Pizza.prototype.getSize = function() {
+  return this.size;
 }
 
 // Front End Logic
@@ -31,7 +56,7 @@ $(document).ready(function() {
     var newPizza = new Pizza(customerFirstName, customerLastName, pizzaToppings, pizzaSize);
     $("#customer-name").text(newPizza.getCustomerName());
     $("#chosen-toppings").text(newPizza.getToppings());
-    $("#chosen-size").text(newPizza.pizzaSize);
-    $("#total-cost").text(newPizza.cost);
+    $("#chosen-size").text(newPizza.getSize());
+    $("#total-cost").text(newPizza.determineCost());
   });
 });
