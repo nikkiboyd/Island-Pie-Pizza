c4ff4921ec03e3fde137dec049b20167cf84fbee
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-15 08:29:39 -0700

    add form with all input fields

---
+++ README.md
@@ -0,0 +1,4 @@
+Program will allow user to select multiple toppings for their Pizza
+Program will allow user to select a size for their Pizza
+Program will determine final cost based on their selections
+Program will return the value of the cost to the user
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 // Back End Logic
-function BankAccount(accountHolder, balance) {
-  this.accountHolder = accountHolder;
-  this.balance = balance;
+function Pizza(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
 }
 
 // Front End Logic
 $(document).ready(function() {
+  $("#submit-order-button").submit(function() {
+    var customerName = $("#input-name").val();
+    var pizzaToppings = $("#select-toppings :selected").text();
+    var pizzaSize = $("#select-size :selected").text();
+    var newPizza = new Pizza(pizzaToppings, pizzaSize);
+    $("#customer-name").text(newTicket.movie);
+    $("#show-time").text(newTicket.mTime);
+    $("#movie-rating").text(newTicket.mRating);
+    $("#total-cost").text(newTicket.cost);
+  });
 });
