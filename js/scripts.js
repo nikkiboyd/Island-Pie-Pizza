34c978dc97d2e902bb5a96a6b30a57ea5acc6d74
Nikki Boyd <boyd.nikki@icloud.com>
2018-06-15 08:04:58 -0700

    add initial html and js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Back End Logic
+function BankAccount(accountHolder, balance) {
+  this.accountHolder = accountHolder;
+  this.balance = balance;
+}
+
+// Front End Logic
+$(document).ready(function() {
+});
